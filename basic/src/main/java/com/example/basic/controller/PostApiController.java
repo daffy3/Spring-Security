19bdffdd85456aa3bb62e80267eab6b5932d4267
Marco <weartstudio@naver.com>
2024-06-18 21:28:35 +0900

    feat: POST API 만들어보기

---
+++ basic/src/main/java/com/example/basic/controller/PostApiController.java
@@ -0,0 +1,19 @@
+package com.example.basic.controller;
+
+import com.example.basic.dto.PostRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController()
+@RequestMapping("/api")
+public class PostApiController {
+
+    @PostMapping("/post")
+    public void post(@RequestBody PostRequest requestData) {
+        System.out.println(requestData);
+    };
+}
